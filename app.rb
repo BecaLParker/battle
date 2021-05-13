e7e0b386da166a46143cff4207735dd013590aa2
BecaLParker <becaparker@gmail.com>
2021-05-13 15:37:58 +0100

    extracts logic to player model layer

--- app.rb
+++ app.rb
@@ -3,8 +3,9 @@
 require 'sinatra/base'
 require 'sinatra/reloader'
 require 'rack'
+require './lib/player'
 
-# Battle class runs the webapp game functionality
+# Battle class is responsible for Controller actions to run the webapp game functionality
 class Battle < Sinatra::Base
   enable :sessions
 
@@ -13,20 +14,20 @@ class Battle < Sinatra::Base
   end
 
   post '/names' do
-    session[:player_1_name] = params[:player_1_name]
-    session[:player_2_name] = params[:player_2_name]
+    $player_1 = Player.new(params[:player_1_name])
+    $player_2 = Player.new(params[:player_2_name])
     redirect '/play'
   end
 
   get '/play' do
-    @player_1_name = session[:player_1_name]
-    @player_2_name = session[:player_2_name]
+    @player_1_name = $player_1.name
+    @player_2_name = $player_2.name
     erb :play
   end
 
   get '/attack' do
-    @player_1_name = session[:player_1_name]
-    @player_2_name = session[:player_2_name]
+    @player_1_name = $player_1.name
+    @player_2_name = $player_2.name
     erb :attack
   end
 end
