f1a1f88f3629c27e9f86243ee58686dea907d68d
BecaLParker <becaparker@gmail.com>
2021-05-14 08:55:43 +0100

    gitignore *.html debugging files

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -9,6 +9,7 @@ feature 'attacking' do
   scenario 'attack on player 2' do
     sign_in_and_play
     click_button('Attack')
+    save_and_open_page
     expect(page).to have_content 'John attacked Becca!!'
   end
 end
